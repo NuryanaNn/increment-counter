9ff61603f2c29dad651c372c8d04f4a7a22e57a7
Nuryana N <nuryan504@gmail.com>
2022-12-03 13:43:49 +0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+    counter.innerText = '0'
+    let updateCounter = () => {
+        let target = +counter.getAttribute('data-target');
+
+        let c = +counter.innerText
+        let increment = target/200
+        if(c < target){
+            counter.innerText = `${Math.ceil(c + increment)}`
+            setTimeout(updateCounter, 1)
+        }else{
+            counter.innerText = target
+        }
+    }
+    updateCounter()
+})
